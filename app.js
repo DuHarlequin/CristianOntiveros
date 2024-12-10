a5f2382081fe7699240720a43269db1bcf59c2b8
DuHarlequin <87832678+DuHarlequin@users.noreply.github.com>
2024-12-10 13:42:57 -0600

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-// Código para el botón de copiar correo
-document.getElementById('copyEmail').addEventListener('click', function() {
-    const email = 'tu-correo@example.com';
-    navigator.clipboard.writeText(email);
-});
